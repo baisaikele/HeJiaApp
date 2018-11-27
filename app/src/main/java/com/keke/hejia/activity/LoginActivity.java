d5455afbd7655719964cd7f9a73a5d8ffefac67b
zhangyu127 <1005696885@qq.com>
2018-11-27 16:40:38 +0800

    提交登录页面及相关依赖和欢迎页面activity

---
+++ app/src/main/java/com/keke/hejia/activity/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.keke.hejia.activity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.mbms.MbmsErrors;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.keke.hejia.R;
+import com.keke.hejia.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void getIntentData() {
+
+    }
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initUI() {
+        initTitle();
+    }
+
+
+    /**
+     * 2018/11/27
+     * zhangyu
+     * 透明式状态栏
+     **/
+    private void initTitle() {
+        setHalfTransparent();
+        setFitSystemWindow(true);
+    }
+
+
+    /**
+     * 如果需要内容紧贴着StatusBar
+     * 应该在对应的xml布局文件中，设置根布局fitsSystemWindows=true。
+     */
+    private View contentViewGroup;
+
+    protected void setFitSystemWindow(boolean fitSystemWindow) {
+        if (contentViewGroup == null) {
+            contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        }
+        contentViewGroup.setFitsSystemWindows(fitSystemWindow);
+    }
+
+    //半透明状态栏
+    protected void setHalfTransparent() {
+
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+}
